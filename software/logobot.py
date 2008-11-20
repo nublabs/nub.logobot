26462fc602666cf3802ef20e1906f27b280ab19d
alec resnick <aresnick@dror.(none)>
2008-11-19 22:06:36 -0500

    Commenting and documenting logobot.py.  Also separated lookup dictionaries for easier understanding.  Adding TODOs

--- software/logobot.py
+++ software/logobot.py
@@ -1,67 +1,104 @@
 #!/usr/bin/python
+""" @package logobot
 
-import serial, socket
-import re, time, math
+logobot.py sends commands to a serial port (or a USB port imitating a serial port) for transmission to logobot, wirelessly controlled robot which can draw and move.  This is a Python wrapper around the serial commands that are sent wirelessly using the XBee protocol.
+"""
+
+import serial
+import socket
+
+import re
+import time
+import math
 
 import logging
 logging.basicConfig(level=logging.DEBUG) 
 # Available levels: NOSET, DEBUG, INFO, WARN, ERROR, CRITITCAL
 
-lengthScale = 1 # number of clicks in one inch, should be 400
-rotationScale = 1 #25.6 # number of clicks in one degree, should be 29
+
+""" number of clicks in one inch, should be 400 """
+lengthScale = 1 
+
+""" 25.6 = the number of clicks in one degree, should be 29 """
+rotationScale = 1 
 
 import os, sys, dircache
 portDirectory = '/dev/'
 portsAvailable = dircache.opendir(portDirectory)
 
+""" Set the usb port and baudrate """
 usb = serial.Serial()
-usb.baudrate = 9600
+usb.baudrate = 19200
 usb.timeout = 0
 
-# Select active USB port
+""" autoselect usb port on linux machines"""
 for port in portsAvailable:
     if re.search('USB', port):
         usb.port = portDirectory + port
         break
 
-# Lookup dictionary 
-lookup = {
+
+""" These lookup dictionaries are separated for convenience and clarity of function.
+communications : contains the definitions for communication protocol messages
+requests       : contains definitions for logobot status or info requests
+statuses       : contains definitions for all the status reports logobot could return
+controls       : contains the definitions of the commands which control logobot's motion and actions 
+errors         : contains all the error messages we use  """
+communications = {
     'WAITING':1,
     'MSG_START':2,
     'COMMAND':3,
+    'MSG_END':4,
+    'COMMAND':3,
     'MSG_END':8,
-    'ACK':9,
-    'REPORTING':13,
-    'DATA_IN':17,
-    'LISTENING_FOR_THE_END':19,
-    'WTF':20,
-    'LISTENING_FOR_A_CHECKSUM':21,
-    'LISTTENING':22,
-    'ACTION_COMPLETE':10,
+    'BAD_CHECKSUM':24,
     'PARAMETER1':11,
-    'PARAMETER2':12,
-    'MOVING':26,
+    'PARAMETER2':12,}
+
+requests = {    
     'GET_STATUS':27,
+    'GET_X':14,
+    'GET_Y':15,
+    'GET_BATT':16,
+    'GET_STATUS':27,}
+
+statuses = {
+    'REPORTING':13,
+    'LISTENING_FOR_THE_END':19,
+    'LISTENING_FOR_A_CHECKSUM':21,
+    'LISTENING':22,
+    'MOVING':26,
+    'ACTION_COMPLETE':10,
+    'DATA_IN':17,
+}
+
+controls = {
     'FORWARD':4,
     'BACKWARDS':5,
     'LEFT':6,
     'RIGHT':7,
-    'GET_X':14,
-    'GET_Y':15,
-    'GET_BATT':16,
-    'INVALID_COMMAND':23,
-    'BAD_CHECKSUM':24,
-    'BAD_MESSAGE_STRUCTURE':25,
-    'MOVING':26,
-    'GET_STATUS':27,
     'PEN_UP':28,
-    'PEN_DOWN':29,
-    }
+    'PEN_DOWN':29,}
 
-# Initializes reverseLookup dictionary for decoding later
+errors = {
+    'WTF':20,
+    'ACK':9,
+    'BAD_MESSAGE_STRUCTURE':25,
+    'INVALID_COMMAND':23,}
+
+
+""" messageDictionaries merges the lookup dictionaries for searching""" 
+messageDictionaries = [communications, requests, statuses, controls, errors]
+
+""" update merges dictionaries together, this merges all the dictionaries from the messageDictionaries array together """
+lookup = reduce(lambda x,y: 
+                    x.update(y),
+                messageDictionaries)
+
+""" The reverseLookup dictionary will eventually hold the reverse mapping of numbers to messages for use in parsing logobot responses """
 reverseLookup = {}
 
-# Defines Scratch interface dictionary
+""" Defines interface to scrape, which interprets Scratch commands and translates """
 scrapeLookup = {
     'turnRight':'RIGHT',
     'turnLeft':'LEFT',
@@ -70,8 +107,11 @@ scrapeLookup = {
     'penDown':'PEN_DOWN',
 }
 
-# Opens connection with serial/USB port
+
+
 def openConnection(port=usb):
+""" open Connection opens a connection on the given port, defaulting to the autolocated usb port and returning a serial object which has been flushed and is ready for reading and writing.
+"""
     ser = usb
     if port.isOpen():
         logging.debug('Port %s already open.', port.port)
@@ -89,9 +129,12 @@ def openConnection(port=usb):
         logging.debug('Port %s open and flushed', port.port)
     return ser
 
-# Translates message for transmission by looking up keyword in dictionary
+
+
 def encode(message):
+""" encode takes a given message and translates it to numbers via the lookup dictionary. """
     logging.debug('Encoding %s', message)
+
     if message == '':
         logging.warn('Message is blank.')
         return chr(0)
@@ -101,12 +144,16 @@ def encode(message):
         # Note that repr is needed to see the hexadecimal.  This can lead to tricky parsing problems.
         return translated
     else:
-        byteRepresentation = chr(message)
         logging.warn('Message neither null nor defined, returning original message %s', repr(byteRepresentation))
+        byteRepresentation = chr(message)
+        
         return byteRepresentation
 
-# Translates Logobot responses
+
 def decode(value):
+""" decode looks up the message it maps to using the reverseLookup dictionary, letting us interpret Logobot commands 
+"""
+
     if len(value) == 0:
         return ''
     else:
@@ -123,38 +170,49 @@ def decode(value):
             reverseLookup[value] = None
             return value
 
-# Wrapper around serial output on port, insures open connection
+
+
 def portWrite(message, port=usb):
+""" portWrite is a wrapper around writing to the serial port that will insure that a port is open before attempting to write to it and log a message while doing so.
+"""
     logging.debug('Writing %s on %s', message, port.port)
     openConnection(port)
     return port.write(message)
 
-# Wrapper around serial read on port, insures open connection
+
+
 def portRead(port=usb):
+""" portRead is a wrapper around writing to the serial port that will insure that a port is open before attempting to read it and log a message while doing so.
+"""
     logging.debug('Reading from %s', port.port)
     openConnection(port)
     return port.read()
 
-# Opening handshake with Logobot
+
 def msgStart(port=usb):
+""" msgStart tells logobot a message is beginning """
     logging.debug('Writing MSG_START byte.')
     portWrite(encode('MSG_START'), port)
 
-# Closing handshake with Logobot
+
 def msgEnd(port=usb):
+""" msgEnd tells Logobot that a message is finished """
     logging.debug('Sending MSG_END byte.')    
     portWrite(encode('MSG_END'),port)
 
-# say is responsible for encoding the message and properly forming it
-# For access to the port raw, use portWrite        
+
 def say(message, port=usb):    
+""" say encodes the message and sends it over the port, well-formed.  If you want to simply send a bit on the port, use portWrite"""
     portWrite(encode(message))
     logging.debug('Wrote %s', repr(message))
     return message
 
-# Reads from serial buffer for responses to decode
+
 def listen(port=usb):
-    time.sleep(0.1)
+""" listen waits for 0.1s and then reads whatever is in the serial buffer and decodes it
+** TODO Write a listen function which waits for a particular message 
+"""
+    time.sleep(0.1) """ arbitrarily chosen interval """
     bufferValue = portRead(port)
     logging.debug('Read %s from port %s', repr(bufferValue), port.port)
     if str(repr(bufferValue)) == '':
@@ -164,8 +222,9 @@ def listen(port=usb):
         decodedMessage = decode(bufferValue)
         return decodedMessage
 
-# Asks Logobot about its various states
+
 def ask(message, port=usb):
+""" ask queries logobot for its status and attributes, flushing the input channel and preparing it to read logobot's response """
     msgStart()
     logging.debug('Flushing input channel. . .')
     usb.flushInput()
@@ -177,8 +236,12 @@ def ask(message, port=usb):
     logging.debug('Read %s from buffer', status)
     return status
 
-# Generic command wrapper which parses and transmits message, complete with handshakes    
+
 def do(command, parameter = '', port=usb):
+""" do will take a command, translate it, pass it on, and make sure it is well-formed.  It is the workhorse of logobot.py, and will send the well-formed message to the USB port, printing whatever response received over the serial port 
+
+** TODO ensuring via checksum that the message is transmitted and received. """
+
     #timeout = port.timeout
     #timeout=parameter/1000
     waitUntilReady(1)
@@ -209,11 +272,15 @@ def do(command, parameter = '', port=usb):
                   listen(), str(command), str(parameter))
     return [command, parameter]
 
-# Waits until Logobot is ready to receive commands
+
+
 def waitUntilReady(timeout=-1):
+""" waitUntilReady
+
+waitUntilReady will pause the script until Logobot is ready to receive commands, so that we do notoverwhelm Logobot with commands while it is still carrying them out """
     if timeout == -1:
         while ask('GET_STATUS') != 'WAITING':
-            time.sleep(0.1)
+            time.sleep(0.1) """ Sleep for 0.1 seconds """
         return
     else:
         timeSlept = 0
@@ -221,13 +288,16 @@ def waitUntilReady(timeout=-1):
         while timeSlept < timeout:
             if ask('GET_STATUS') == 'WAITING':
                 time.sleep(0.5)
-                return
+                 return
             else:
                 time.sleep(timeoutInterval)
                 timeSlept += timeoutInterval
         
-# Simple checksum to verify packet integrity
+
 def checksum(command, parameter):
+""" checksum
+
+"""
     logging.debug('Computing checksum of %s and %s', command, parameter)
     cmdValue = ord(encode(command))
     if parameter < 256:
@@ -239,8 +309,12 @@ def checksum(command, parameter):
 
     return checksumValue
 
-# Wrapper around turning n degrees.  Positive is clockwise, negative counterclockwise
+
 def turn(degrees):
+""" turn
+
+turn is a wrapper around turning n degrees.  Positive is clockwise, negative counterclockwise
+"""
     angularClicks = abs(degrees)*rotationScale
     if degrees < 0:
         logging.info('Turning left %d degrees', abs(degrees))
@@ -249,8 +323,11 @@ def turn(degrees):
         logging.info('Turning right %d degrees', abs(degrees))
         do('RIGHT', angularClicks)
 
-# Wrapper around translation
+
 def move(steps):
+""" move
+
+Wrapper around translation """
     lengthClicks = abs(steps)*lengthScale
     if steps < 0:
         logging.info('Moving backwards %d steps', steps)
@@ -259,8 +336,11 @@ def move(steps):
         logging.info('Moving forward %d steps', steps)
         do('FORWARD', lengthClicks)
 
-# Interface to Scratch customization
+
 def scrapeInterface(command, parameter):
+""" scrapeInterface
+
+Interface to Scratch customization """
     translatedCommand = ''
     translatedParameter = ''
     logging.debug('Executing scrapeInterface with %s and %s', command, parameter)
@@ -288,9 +368,13 @@ def scrapeInterface(command, parameter):
 
     return [translatedCommand, translatedParameter]
 
-# Queues up jobs to prevent Logobot from overflowing
-# TODO: handle multiple sets of jobs being sent (implement threading)
+
 def queue(messages):
+"""
+queue
+
+# Queues up jobs to prevent Logobot from overflowing
+# TODO: handle multiple sets of jobs being sent (implement threading) """
     for message in messages:
         logging.debug('Trying to use message %s', message)
         originalCommand, originalParameter = message[0], message[1]
